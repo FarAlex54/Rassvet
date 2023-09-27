40cdd52048b8670fdf8842d265344ff3169efb92
FarAlex54 <sibacadem@mail.ru>
2023-09-27 23:16:58 +0700

    Туц

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
@@ -9,12 +10,15 @@ const Header = () => {
     <div>
       <Navbar bg="primary" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Главная</Navbar.Brand>
+          <Navbar.Brand href="#home"><p className='classFont'>Главная</p></Navbar.Brand>
           <Nav className="me-auto classFont">
             <Nav.Link href="#home">Об СНТ Рассвет</Nav.Link>
             <Nav.Link href="#News">Новости</Nav.Link>
             <Nav.Link href="#BuySell">Купи/Продай</Nav.Link>
-            <Nav.Link href="#About">Учредительные документы</Nav.Link>
+            <Nav.Link href="#Questions">Вопросы</Nav.Link>
+            <Nav.Link href="#Contacts">Контакты</Nav.Link>
+            <Nav.Link href="#About">Документы</Nav.Link>
+            <Nav.Item><Button variant='success'><p className='classFont'>Личный кабинет</p></Button></Nav.Item>
           </Nav>
         </Container>
       </Navbar>
