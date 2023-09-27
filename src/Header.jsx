b1461ba10e5dc5ae0cf7287cd9722a0dc8f9e4cf
FarAlex54 <sibacadem@mail.ru>
2023-09-27 21:57:04 +0700

    Изменения

---
+++ src/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Главная</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Об СНТ Рассвет</Nav.Link>
+            <Nav.Link href="#News">Новости</Nav.Link>
+            <Nav.Link href="#BuySell">Купи/Продай</Nav.Link>
+            <Nav.Link href="#About">Учредительные документы</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header
