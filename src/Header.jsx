752e1da47553e10d12eb5ea6a65a49de1d924992
FarAlex54 <sibacadem@mail.ru>
2023-09-29 19:50:17 +0700

    Обновил header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,18 +9,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Header = () => {
   return (
     <div>
-      <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home"><p className='classFont'>Главная</p></Navbar.Brand>
-          <Nav className="me-auto classFont">
-            <Nav.Link href="#home">Об СНТ Рассвет</Nav.Link>
-            <Nav.Link href="#News">Новости</Nav.Link>
-            <Nav.Link href="#BuySell">Купи/Продай</Nav.Link>
-            <Nav.Link href="#Questions">Вопросы</Nav.Link>
-            <Nav.Link href="#Contacts">Контакты</Nav.Link>
-            <Nav.Link href="#About">Документы</Nav.Link>
-            <Nav.Item><Button variant='success'><p className='classFont'>Личный кабинет</p></Button></Nav.Item>
-          </Nav>
+      <Navbar fixed="top" bg="dark" data-bs-theme="dark" expand="md" className="bg-body-tertiary">
+        <Container className=''>
+          <Navbar.Brand href="#home" className='classFont'>Лого</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="" id="basic-navbar-nav">
+            <Nav className="me-auto classFont">
+              <Nav.Link href="#home">Об СНТ Рассвет</Nav.Link>
+              <Nav.Link href="#News">Новости</Nav.Link>
+              <Nav.Link href="#BuySell">Купи/Продай</Nav.Link>
+              <Nav.Link href="#Questions">Вопросы</Nav.Link>
+              <Nav.Link href="#Contacts">Контакты</Nav.Link>
+              <Nav.Link href="#About">Документы</Nav.Link>
+            </Nav>
+            <Nav>
+              <Navbar.Text><Button variant='success'>Личный кабинет</Button></Navbar.Text>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
